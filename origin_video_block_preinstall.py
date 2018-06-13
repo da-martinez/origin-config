343e747d783777b8276042e3ca604bb4822208b3
James Majors <jamesmajors@jamess-mbp-3.turner.com>
2018-06-13 15:59:02 -0400

    Changes to formatting in preinstall

--- origin_video_block_preinstall.py
+++ origin_video_block_preinstall.py
@@ -9,29 +9,29 @@ package_file = '../../package.json'
 
 with open(package_file) as json_file:
     data = json.load(json_file)
-    # print(data)
 
 
 ###########################
 # Add Dependencies to React Native project
 ###########################
 print("\nAdding Dependencies to React Native Project")
+print("-------------------------------------------")
+
 dependencies = data["dependencies"]
 dependencies["origin-react-native-video-player"] = "git+ssh://git@github.com/turnercode/origin-video-block.git"
 dependencies["mod-pbxproj"] = "git@github.com:noizetoys/mod-pbxproj.git"
 data["dependencies"] = dependencies
-# print("Dependencies = %s" % data["dependencies"])
 
 
 ##########################
 # Add Pre and Post Install Scripts
 ##########################
 print("\nAdding Pre and Post Install scripts")
+print("-----------------------------------")
 scripts = data["scripts"]
 scripts["preconfigure"] = "react-native link"
 scripts["configure"] =  "python origin_video_block_postinstall.py"
 scripts["postinstall"] = "npm run configure"
-# print("Scripts = %s" % data["scripts"])
 
 
 with open(package_file, 'w') as json_file:
@@ -43,7 +43,9 @@ with open(package_file, 'w') as json_file:
 ##########################
 file_to_copy ="origin_video_block_postinstall.py"  
 print("\nCopying %s to project Directory" % file_to_copy)
+print("------------------------------------------------------------------")
 copy2(file_to_copy, "../../" + file_to_copy)
 
-
-print("\n\nTo complete installation of Video player, please type 'npm install'\n\n")
+print("\n\n------------------------------------------------------------------")
+print("To complete installation of Video player, please type 'npm install'")
+print("------------------------------------------------------------------")
